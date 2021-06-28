c0e8afe58bcec8bd33508fab279bb42ff13dded6
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-06-28 00:27:53 -0400

    added html, css, and js

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 //parse incoming json data
 app.use(express.json());
+//front end html request have access to js and style
+app.use(express.static('public'));
 
 function filterByQuery(query, animalArray) {
     let personalityTraitsArray = [];
@@ -107,7 +109,9 @@ app.post('/api/animals', (req,res) => {
         res.json(animal);
     }   
 })
-
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'./public/index.html'));
+})
 app.listen(PORT, () => {
     console.log('API server now on port 3001!!');
 })
